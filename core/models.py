fffc0bf1e9f594cf242797b944ecac425f324f2c
amirsalar-hoseini <hosseiniseyedamirsalar@gmail.com>
2025-07-21 12:57:43 +0330

    Done

--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
+import uuid
 
-# Create your models here.
+class Movie(models.Model):
+    GENRE_CHOICES = [
+        ('action', 'Action'),
+        ('comedy', 'Comedy'),
+        ('drama', 'Drama'),
+        ('horror', 'Horror'),
+        ('romance', 'Romance'),
+        ('science_fiction', 'Science Fiction'),
+        ('fantasy', 'Fantasy'),
+    ]
+    uu_id = models.UUIDField(default=uuid.uuid4)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
+    length = models.PositiveIntegerField()
+    image_card = models.ImageField(upload_to='images/')
+    image_cover = models.ImageField(upload_to='images/')
+    video = models.FileField(upload_to='videos/')
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class MovieList(models.Model):
+    owner_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
--- core/views.py
+++ core/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render, redirect
+import re
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import Movie, MovieList
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+
 
 def index(request):
-    return render(request, 'index.html')
+    movies = Movie.objects.all()
+    featured_movie = movies[len(movies)-1]
+
+    context = {
+        'movies': movies,
+        'featured_movie': featured_movie,
+    }
+    return render(request, 'index.html', context)
 
 def login(request):
     if request.method == "POST":
@@ -43,3 +55,72 @@ def signup(request):
     else:
         pass
     return render(request, 'signup.html')
+
+@login_required(login_url='login')
+def movie(request, pk):
+    movie = Movie.objects.get(uu_id=pk)
+
+    context = {
+        'movie_details': movie,
+    }
+    return render(request, 'movie.html', context)
+
+@login_required(login_url='login')
+def my_list(request):
+    movie_list = MovieList.objects.filter(owner_user=request.user)
+    user_movie_list = []
+    for movie in movie_list:
+        user_movie_list.append(movie.movie)
+
+    context = {
+        'movies': user_movie_list,
+    }
+    return render(request, 'my_list.html', context)
+
+@login_required(login_url='login')
+def add_to_list(request):
+    if request.method == 'POST':
+        movie_id = request.POST['movie_id']
+        uuid_pattern = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+        match = re.search(uuid_pattern, movie_id)
+        movie_id = match.group() if match else None
+        movie = get_object_or_404(Movie, uu_id=movie_id)
+        movie_list, created = MovieList.objects.get_or_create(owner_user=request.user, movie=movie)
+
+        if created:
+            response_data = {'status': 'success', 'message': 'Movie added ✓'}
+        else:
+            response_data = {'status': 'info', 'message': 'Movie already in list'}
+        return JsonResponse(response_data)
+    else:
+        response_data = {'status': 'error', 'message': 'Invalid request'}
+        return JsonResponse(response_data, status=400)
+
+@login_required(login_url='login')
+def search(request):
+    if request.method == 'POST':
+        search = request.POST['search_term']
+        movies = Movie.objects.filter(title__icontains=search)
+
+        context = {
+            'movies': movies,
+            'search_term': search,
+        }
+    else:
+        return redirect('/')
+    return render(request, 'search.html', context)
+
+@login_required(login_url='login')
+def genre(request, pk):
+    movies = Movie.objects.filter(genre=pk)
+
+    context = {
+        'movies': movies,
+        'movie_genre': pk,
+    }
+    return render(request, 'genre.html', context)
+
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
